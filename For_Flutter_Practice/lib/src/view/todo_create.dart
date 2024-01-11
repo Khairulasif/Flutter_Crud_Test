32773f801ab08dd71a2950a3bbd3212c51043059
Md. Khairul Islam <104551563+Khairulasif@users.noreply.github.com>
2024-01-11 17:39:09 +0600

    commit

--- For_Flutter_Practice/lib/src/view/todo_create.dart
+++ For_Flutter_Practice/lib/src/view/todo_create.dart
@@ -1,15 +1,67 @@
 import 'package:flutter/material.dart';
 
-class TodoCreate extends StatelessWidget {
+class TodoCreate extends StatefulWidget {
   const TodoCreate({super.key});
 
+  @override
+  State<TodoCreate> createState() => _TodoCreateState();
+}
+
+class _TodoCreateState extends State<TodoCreate> {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.red,
       appBar: AppBar(
-        title: Text("Todo List"),
+        title: Text("Todo Create"),
       ),
+      body: Form(
+        key: formKey,
+          child: Column(
+            children: [
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: 400,
+                padding: EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: TextFormField(
+                  minLines: 3,
+                  maxLines: 3,
+                  validator: (value) {
+                    if(value == null || value.isEmpty) {
+                      return 'Required Field';
+                    }
+                    return null;
+                  },
+                  style: const TextStyle(color: Colors.red),
+                  decoration: const InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "Enter Description",
+                    hintStyle: TextStyle(color: Colors.grey),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              Container(
+                width: 400,
+                height: 55,
+                child: const Text(
+                  "Save",
+                  style: TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+                ),
+
+              )
+            ],
+          )),
     );
   }
 }
+
